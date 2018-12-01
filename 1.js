224b298e43d4888ec53a42a7ccfcdc76881eb5c9
Maciej Zawilski <maciejzawilski@Maciejs-MacBook-Pro.local>
2018-12-01 12:19:19 +0100

    First day second challenge

--- 1.js
+++ 1.js
@@ -1,38 +1,66 @@
 const fs = require('file-system');
 
-const checkArray = (arr, value) => {
-			console.log(value);
-	arr.forEach((element) => {
-		if (element === value){
-			return true;
-		}
-	});
-	return false;
-}
+// What is the final frequency if we know the values of fluctuations?
 
 const question = () => {
 	fs.readFile('./1.txt', (err, data) => {
 		const frequencyChanges = data.toString().split('\n');
-		const previousFrequencies = [];
 		const answer = frequencyChanges.reduce((acc, currentValue) => {
 			const sign = currentValue.substr(0,1);
 			const value = Number(currentValue.substr(1));
 			if (sign === '-') {
-				acc -= value;
-				checkArray(previousFrequencies, acc) ? 1 : previousFrequencies.push(acc);
-				return acc;
+				return acc -= value;
 			} else {
-				acc += value;
-				checkArray(previousFrequencies, acc) ? 1 : previousFrequencies.push(acc);
-				return acc;
+				return acc += value;
 			}
 		}, 0)
-		console.log(previousFrequencies);
 		console.log('Last frequency: ', answer);
 	})
 }
 
 question();
 
-// dodaj liczbę do array
-// sprawdź array
+// What is the first frequency that we reach twice, considering that
+// fluctuations happen periodically with the same set of values? 
+
+// Possible improvement: change array to binary search tree 
+const checkArray = (arr, value) => {
+	let check = false;
+	arr.forEach((element) => {
+		if (element === value){
+			check = true;
+		}
+	})
+	return check;
+}
+
+const question2 = () => {
+	fs.readFile('./1.txt', (err, data) => {
+		const frequencyChanges = data.toString().split('\n');
+		const previousFrequencies = [];
+		let accumulator = 0;
+		let end = false;
+		while(!end){
+			const answer = frequencyChanges.some((currentValue) => {
+				const sign = currentValue.substr(0,1);
+				const value = Number(currentValue.substr(1));
+				if (sign === '-') {	
+						accumulator -= value;
+						if (checkArray(previousFrequencies, accumulator)) {
+							end = true;
+							return true;
+						} else previousFrequencies.push(accumulator);
+				} else {
+					accumulator += value;
+					if (checkArray(previousFrequencies, accumulator)) {
+						end = true;
+						return true;
+					} else previousFrequencies.push(accumulator);
+				}
+			})
+		}
+		console.log('First double frequency: ', accumulator);
+	})
+}
+
+question2();
