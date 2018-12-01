4d41b3008bb80eee052c2fdc8986e8024630e835
Maciej Zawilski <maciejzawilski@Maciejs-MacBook-Pro.local>
2018-12-01 11:29:23 +0100

    question changed

--- 1.js
+++ 1.js
@@ -1,19 +1,38 @@
 const fs = require('file-system');
 
-function question() {
-	fs.readFile('./1a.txt', (err, data) => {
+const checkArray = (arr, value) => {
+			console.log(value);
+	arr.forEach((element) => {
+		if (element === value){
+			return true;
+		}
+	});
+	return false;
+}
+
+const question = () => {
+	fs.readFile('./1.txt', (err, data) => {
 		const frequencyChanges = data.toString().split('\n');
+		const previousFrequencies = [];
 		const answer = frequencyChanges.reduce((acc, currentValue) => {
 			const sign = currentValue.substr(0,1);
 			const value = Number(currentValue.substr(1));
 			if (sign === '-') {
-				return acc -= value;
+				acc -= value;
+				checkArray(previousFrequencies, acc) ? 1 : previousFrequencies.push(acc);
+				return acc;
 			} else {
-				return acc += value;
+				acc += value;
+				checkArray(previousFrequencies, acc) ? 1 : previousFrequencies.push(acc);
+				return acc;
 			}
 		}, 0)
-		console.log(answer);
+		console.log(previousFrequencies);
+		console.log('Last frequency: ', answer);
 	})
 }
 
 question();
+
+// dodaj liczbę do array
+// sprawdź array
