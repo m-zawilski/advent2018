2c5756cd734843b21afa9754f186586185849b1e
Maciej Zawilski <maciejzawilski@Maciejs-MacBook-Pro.local>
2018-12-11 11:08:09 +0100

    11th

---
+++ 11.js
@@ -0,0 +1,149 @@
+// --- Day 11: Chronal Charge ---
+// You watch the Elves and their sleigh fade into the distance as they head toward the North Pole.
+
+// Actually, you're the one fading. The falling sensation returns.
+
+// The low fuel warning light is illuminated on your wrist-mounted device. Tapping it once causes it to project a hologram of the situation: a 300x300 grid of fuel cells and their current power levels, some negative. You're not sure what negative power means in the context of time travel, but it can't be good.
+
+// Each fuel cell has a coordinate ranging from 1 to 300 in both the X (horizontal) and Y (vertical) direction. In X,Y notation, the top-left cell is 1,1, and the top-right cell is 300,1.
+
+// The interface lets you select any 3x3 square of fuel cells. To increase your chances of getting to your destination, you decide to choose the 3x3 square with the largest total power.
+
+// The power level in a given fuel cell can be found through the following process:
+
+// Find the fuel cell's rack ID, which is its X coordinate plus 10.
+// Begin with a power level of the rack ID times the Y coordinate.
+// Increase the power level by the value of the grid serial number (your puzzle input).
+// Set the power level to itself multiplied by the rack ID.
+// Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+// Subtract 5 from the power level.
+// For example, to find the power level of the fuel cell at 3,5 in a grid with serial number 8:
+
+// The rack ID is 3 + 10 = 13.
+// The power level starts at 13 * 5 = 65.
+// Adding the serial number produces 65 + 8 = 73.
+// Multiplying by the rack ID produces 73 * 13 = 949.
+// The hundreds digit of 949 is 9.
+// Subtracting 5 produces 9 - 5 = 4.
+// So, the power level of this fuel cell is 4.
+
+// Here are some more example power levels:
+
+// Fuel cell at  122,79, grid serial number 57: power level -5.
+// Fuel cell at 217,196, grid serial number 39: power level  0.
+// Fuel cell at 101,153, grid serial number 71: power level  4.
+// Your goal is to find the 3x3 square which has the largest total power. The square must be entirely within the 300x300 grid. Identify this square using the X,Y coordinate of its top-left fuel cell. For example:
+
+// For grid serial number 18, the largest total 3x3 square has a top-left corner of 33,45 (with a total power of 29); these fuel cells appear in the middle of this 5x5 region:
+
+// -2  -4   4   4   4
+// -4   4   4   4  -5
+//  4   3   3   4  -4
+//  1   1   2   4  -3
+// -1   0   2  -5  -2
+// For grid serial number 42, the largest 3x3 square's top-left is 21,61 (with a total power of 30); they are in the middle of this region:
+
+// -3   4   2   2   2
+// -4   4   3   3   4
+// -5   3   3   4  -4
+//  4   3   3   4  -3
+//  3   3   3  -5  -1
+// What is the X,Y coordinate of the top-left fuel cell of the 3x3 square with the largest total power?
+
+// --- Part Two ---
+// You discover a dial on the side of the device; it seems to let you select a square of any size, not just 3x3. Sizes from 1x1 to 300x300 are supported.
+
+// Realizing this, you now must find the square of any size with the largest total power. Identify this square by including its size as a third parameter after the top-left coordinate: a 9x9 square with a top-left corner of 3,5 is identified as 3,5,9.
+
+// For example:
+
+// For grid serial number 18, the largest total square (with a total power of 113) is 16x16 and has a top-left corner of 90,269, so its identifier is 90,269,16.
+// For grid serial number 42, the largest total square (with a total power of 119) is 12x12 and has a top-left corner of 232,251, so its identifier is 232,251,12.
+// What is the X,Y,size identifier of the square with the largest total power?
+
+const getPoints = (x, y, serialNumber) => {
+	let rackID = x + 10;
+	return Math.floor(((rackID*y+serialNumber)*rackID/100)%10)-5;
+}
+
+const getCell = (x, y, serialNumber) => {
+	return {
+		x: x,
+		y: y,
+		power: getPoints(x, y, serialNumber)
+	}
+}
+
+const getAllCells = (serialNumber, boardSize) => {
+	const newCells = [];
+	for (let y = 1; y<=boardSize; y++){
+		for (let x = 1; x<=boardSize; x++){
+			newCells.push(getCell(x,y,serialNumber));
+		}
+	}
+	return newCells;
+}
+
+const getSquare = (cell, i, cells, size, boardSize) => {
+	if(cell.x>boardSize-size+1 || cell.y>boardSize-size+1){
+		return null;
+	} 
+	let power = 0;
+	for (let x = 0; x<size; x++){
+		for (let y = 0; y<size; y++){
+			power += cells[i+boardSize*x+y].power;
+		}
+	}
+	return {
+		cell: cell,
+		power: power,
+		size: size
+	}
+}
+
+const getPossibleSquares = (cell, i, cells, boardSize) => {
+	const array = [];
+	const maxSize = Math.min(boardSize+1-cell.x, boardSize+1-cell.y)
+	for(let size = 1; size < maxSize; size++){
+		let square = getSquare(cell, i, cells, size, boardSize);
+		if(square === null) {return null}
+		array.push(square);
+	}
+	return array;
+}
+
+const getAllSquares = (cells, boardSize) => {
+	const squares = [];
+	cells.map((cell, i, array) => {
+		let newSquares = getPossibleSquares(cell, i, array, boardSize);
+		if (newSquares !== null) squares.push(...newSquares);
+		console.log(cell.x, cell.y);
+	})
+	return squares;
+}
+
+const findBiggestSquare = (cells, boardSize) => {
+	const squares = getAllSquares(cells, boardSize);
+	let index = 0;
+	squares.reduce((max, square, i) => {
+		if(square.power > max){
+			max = square.power;
+			index = i;
+		} 
+		return max;
+	}, -Infinity)
+	const biggestSquare = squares[index];
+	return biggestSquare;
+}
+
+const question = () => {
+	const serialNumber = 1718;
+	const boardSize = 300;
+	const cells = getAllCells(serialNumber, boardSize);
+	const biggestSquare = findBiggestSquare(cells, boardSize);
+	const answer = biggestSquare.cell;
+	console.log(`The top-left fuel cell of the square with largest total power is ${answer.x},${answer.y}.`)
+	console.log(`Square's size is ${biggestSquare.size} and it's power is ${biggestSquare.power}.`)
+}
+
+question();
