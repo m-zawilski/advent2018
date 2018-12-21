dc5e87f2d1c8576fce52da46a9bea1de9a4967e2
Maciej Zawilski <maciejzawilski@Maciejs-MacBook-Pro.local>
2018-12-21 15:58:17 +0100

    19th

--- 18/18.js
+++ 18/18.js
@@ -10,66 +10,121 @@ const getElement = (x, y, type) => {
 
 const getScore = (map) => {
 	let treesCounter = 0, lumberyardsCounter = 0;
-	map.map((el) => {
-		if(el.type==='#') lumberyardsCounter++;
-		if(el.type==='|') treesCounter++;
+	map.map((field) => {
+		if(field.type==='#') lumberyardsCounter++;
+		if(field.type==='|') treesCounter++;
 	})
 	return treesCounter*lumberyardsCounter;
 }
 
-const question1 = () => {
+const createMap = (data) => {
+	const mapData = data.toString().split('\n').map((line) => {
+		return line.split('');
+	})
+	const newMap = [];
+	mapData.map((rows, y) => {
+		rows.map((cell, x) => {
+			newMap.push(getElement(x, y, cell));
+		})
+	})
+	return newMap;
+}
+
+const getNeighbours = (field, map) => {
+	const neighbours = [];
+	map.map((otherField) => {
+		const distance = Math.sqrt(Math.pow(otherField.x-field.x,2)
+										+Math.pow(otherField.y-field.y,2))
+		if(distance<2 && distance>0){
+			neighbours.push(otherField);
+		}
+	})
+	return neighbours;
+}
+
+const checkATree = (field, neighbours) => {
+	let count = 0;
+	neighbours.map((nbr) => {
+		if(nbr.type === '#'){count++;}
+	})
+	if(count>=3){field.type='#';}
+}
+
+const checkALumbyard = (field, neighbours) => {
+	let isTree = false, isLumbyard = false;
+	neighbours.map((nbr) => {
+		if(nbr.type === '|'){isTree = true};
+		if(nbr.type === '#'){isLumbyard = true};
+	})
+	if(!isTree || !isLumbyard){field.type='.';}
+}
+
+const checkOpen = (field, neighbours) => {
+	let count = 0;
+	neighbours.map((nbr) => {
+		if(nbr.type === '|'){count++;}
+	})
+	if(count>=3){field.type='|';}
+}
+
+const runATurn = (map) => {
+	const mapCopy = JSON.parse(JSON.stringify(map));
+	return mapCopy.map((field) => {
+		const neighbours = getNeighbours(field, map);
+		if(field.type === '|'){
+			checkATree(field, neighbours);
+		} else if (field.type === '#'){
+			checkALumbyard(field, neighbours);
+		} else {
+			checkOpen(field, neighbours);
+		}
+		return field;
+	})
+}
+
+const question = (number) => {
 	fs.readFile('./18.txt', (err, data) => {
-		const linesOfData = data.toString().split('\n');
-		const field = linesOfData.map((line) => {
-			return line.split('');
-		})
-		let map = [];
-		field.map((rows, y) => {
-			rows.map((cell, x) => {
-				map.push(getElement(x, y, cell));
-			})
-		})
-		for(let i = 1; i<50000; i++){
-			map = map.map((el, ind1) => {
-				let type = el.type;
-				const neighbours = [];
-				map.map((el2, ind2) => {
-					if(ind1===ind2) return;
-					if(Math.sqrt(Math.pow(el2.x-el.x,2)+Math.pow(el2.y-el.y,2))<2){
-						neighbours.push(el2);
-					}
-				})
-				if(el.type==='|'){
-					let count = 0;
-					neighbours.map((nbr) => {
-						if(nbr.type === '#'){count++;}
-					})
-					if(count>=3){type='#';}
-				} else if (el.type === '#'){
-					let hasTree = false, hasLumberyard = false;
-					neighbours.map((nbr) => {
-						if(nbr.type === '|'){hasTree = true};
-						if(nbr.type === '#'){hasLumberyard = true};
-					})
-					if(!hasTree || !hasLumberyard){type='.';}
-				} else {
-					let count = 0;
-					neighbours.map((nbr) => {
-						if(nbr.type === '|'){count++;}
-					})
-					if(count>=3){type='|';}
-				}
-				return getElement(el.x, el.y, type);
-			})
-			let prevScore = 0;
-			if(i%100 === 0){
-				let newScore = getScore(map);
-				console.log(i, newScore - prevScore);
-				console.log(newScore);
-				prevScore = newScore;
+		let map = createMap(data);
+		if(number === 1){
+			for(let i = 0; i<10; i++){
+				map = runATurn(map);
 			}
+			console.log(`The score after 10 turns is ${getScore(map)}.`);
+		}
+		if(number === 2){
+			let patternFound = false;
+			const mapResults = [];
+			mapResults.push(getScore(map));
+			let patternStart, patternPeriod;
+			while(!patternFound){
+				map = runATurn(map);
+				const result = getScore(map);
+				if(mapResults.indexOf(result) !== -1){
+					const firstResultIndex = mapResults.indexOf(result);
+					const secondResultIndex = mapResults.length;
+					mapResults.push(result);
+					map = runATurn(map);
+					nextResult = getScore(map);
+					if(mapResults.indexOf(nextResult) === firstResultIndex+1){
+						patternFound = true;
+						patternStart = firstResultIndex;
+						patternPeriod = secondResultIndex - firstResultIndex;
+					}
+					else {
+						mapResults.push(nextResult);
+					}
+				} else {
+					mapResults.push(result);
+				}
+			}
+			const whichInPeriod = (1000000000-patternStart)%patternPeriod;
+			console.log(`The result after 1000000000 turns is ${mapResults[whichInPeriod+patternStart]}`);
 		}
 	})
 }
 
-question1();
+question(1);
+question(2);
+
+
+
---
+++ 19/19.lib.js
@@ -0,0 +1,233 @@
+const addr = (A, B, C, registers) => {
+	registers[C] = registers[A] + registers[B];
+	return registers;
+}
+
+const addi = (A, B, C, registers) => {
+	registers[C] = registers[A] + B;
+}
+
+const mulr = (A, B, C, registers) => {
+	registers[C] = registers[A] * registers[B];
+}
+
+const muli = (A, B, C, registers) => {
+	registers[C] = registers[A] * B;
+}
+
+const banr = (A, B, C, registers) => {
+	registers[C] = registers[A] & registers[B];
+}
+
+const bani = (A, B, C, registers) => {
+	registers[C] = registers[A] & B;
+}
+
+const borr = (A, B, C, registers) => {
+	registers[C] = registers[A] | registers[B];
+}
+
+const bori = (A, B, C, registers) => {
+	registers[C] = registers[A] | B;
+}
+
+const setr = (A, B, C, registers) => {
+	registers[C] = registers[A];
+}
+
+const seti = (A, B, C, registers) => {
+	registers[C] = A;
+}
+
+const gtir = (A, B, C, registers) => {
+	registers[C] = (A>registers[B]) ? 1 : 0;
+}
+
+const gtri = (A, B, C, registers) => {
+	registers[C] = (registers[A]>B) ? 1 : 0;
+}
+
+const gtrr = (A, B, C, registers) => {
+	registers[C] = (registers[A]>registers[B]) ? 1 : 0;
+}
+
+const eqir = (A, B, C, registers) => {
+	registers[C] = (A===registers[B]) ? 1 : 0;
+}
+
+const eqri = (A, B, C, registers) => {
+	registers[C] = (registers[A]===B) ? 1 : 0;
+}
+
+const eqrr = (A, B, C, registers) => {
+	registers[C] = (registers[A]===registers[B]) ? 1 : 0;
+}
+
+
+const readData = (data) => {
+	const pointer = data[0].split(' ')[1];
+	const instructionLines = data.splice(1,data.length-1).map((ins) => {
+		const instructionLine = ins.split(' ');
+		return {
+			name: instructionLine[0],
+			firstArg: Number(instructionLine[1]),
+			secondArg: Number(instructionLine[2]),
+			thirdArg: Number(instructionLine[3]),
+		}
+	})
+	return {
+		pointer: pointer,
+		instructionLines: instructionLines
+	}
+}
+
+const runInstruction = (pointer, instructionLines, registers) => {
+	const instructionNames = [addi, addr, mulr, muli, banr, bani,
+													  borr, bori, setr, seti, gtir, gtri,
+								 						gtrr, eqir, eqri, eqrr];
+	const instructionNumber = registers[pointer];
+	instructionNames.some((ins) => {
+		const currentLine = instructionLines[instructionNumber];
+		if(ins.name === currentLine.name){
+			ins(currentLine.firstArg, currentLine.secondArg, currentLine.thirdArg, registers);
+			registers[pointer]++;
+		}
+	})
+	return registers;
+}
+
+const getFinalRegisters = (pointer, instructionLines, registers) => {
+	while(registers[pointer] <= instructionLines.length){
+		registers = runInstruction(pointer, instructionLines, registers);
+		console.log(registers);
+	}
+	return registers;
+}
+
+const getFinalRegistersCheck = (pointer, instructionLines, registers) => {
+	let found = false;
+	for(let x = 0; x<10000; x++){
+		registers = runInstruction(pointer, instructionLines, registers);
+		if(registers[pointer] > instructionLines.length) found = true;
+	}
+	return found;
+}
+
+const areSomeRegistersLooped = (noOfCalls) => {
+	let loopFound = false;
+	noOfCalls.map((number) => {
+		if(number > 100){
+			if(!noOfCalls.some((otherNumber) => {	//to return start of a loop 
+				if (otherNumber+1 === number) return true;
+			})){
+				loopFound = true;
+			}
+		}
+	})
+	return loopFound;
+}
+
+const countCalls = (pointer, instructionLines, registers) => {
+	const noOfCalls = [];
+	let loopFound = false;
+	for(let i = 0; i<instructionLines.length; i++){
+		noOfCalls.push(0);
+	}
+	while(!loopFound){
+		registers = runInstruction(pointer, instructionLines, registers);
+		noOfCalls[registers[pointer]]++;
+		loopFound = areSomeRegistersLooped(noOfCalls)
+	}
+	return {
+		instructionsCalls: noOfCalls,
+		loopedRegisters: registers
+	}
+}
+
+const getLoopLength = (pointer, instructionLines, loopedRegisters) => {
+	const loop = [];
+	let firstInstruction = loopedRegisters[pointer];
+	loop.push(firstInstruction);
+	loopedRegisters = runInstruction(pointer, instructionLines, loopedRegisters);
+	let secondInstruction = loopedRegisters[pointer];
+	loop.push(secondInstruction);
+	let loopFound = false;
+	while(!loopFound) {
+		loopedRegisters = runInstruction(pointer, 
+																			instructionLines, loopedRegisters);
+		let instructionNumber = loopedRegisters[pointer];
+		if(instructionNumber === firstInstruction){
+			loopedRegisters = runInstruction(pointer, 
+																				instructionLines, loopedRegisters);
+			let nextInstructionNumber = loopedRegisters[pointer];
+			if(nextInstructionNumber === secondInstruction) {
+				loopFound = true;
+			} else {
+				loop.push(instructionNumber);
+				loop.push(nextInstructionNumber);
+			}
+		} else {
+			loop.push(instructionNumber);
+		}
+	}
+	return loop.length;
+}
+
+const checkOneLoopChange = (pointer, instructionLines, loopLength, loopedRegisters) => {
+	const beginning = JSON.parse(JSON.stringify(loopedRegisters));
+	for(let i = 0; i<loopLength;i++){
+		loopedRegisters = runInstruction(pointer, instructionLines, loopedRegisters);
+	}
+	const end = JSON.parse(JSON.stringify(loopedRegisters));
+	const result = [];
+	for(let i = 0; i<loopedRegisters.length; i++){
+		result.push(end[i]-beginning[i]);
+	}
+	return result;
+}
+
+const question1 = (data) => {
+	const { pointer, instructionLines } = readData(data.toString().split('\n'));
+	let registers = [0,0,0,0,0,0];
+	const finalRegisters = getFinalRegisters(pointer, instructionLines, registers);
+	return finalRegisters[0];
+}
+
+const testRegisters = (pointer, instructionLines, registers) => {
+	const { instructionsCalls, loopedRegisters } = countCalls(pointer, instructionLines, registers);
+	const loopLength = getLoopLength(pointer, instructionLines, loopedRegisters);
+	const resultsChangeAfterLoop = checkOneLoopChange(pointer, 
+																		instructionLines, loopLength, loopedRegisters);
+}
+
+const findDivisors = (number) => {
+	const divisors = [];
+	for(let i = 1; i<=number; i++){
+		if(number%i === 0) divisors.push(i);
+	}
+	return divisors;
+}
+
+const question2 = (data) => {
+	const { pointer, instructionLines } = readData(data.toString().split('\n'));
+	let registers = [1,0,0,0,0,0];
+	let { instructionsCalls, loopedRegisters } = countCalls(pointer, instructionLines, registers);
+	//testRegisters(pointer, instructionLines, loopedRegisters);
+	const bigNumber = loopedRegisters.reduce((sum, reg) => {
+		if(reg>sum){
+			sum = reg;
+		} return sum;
+	}, 0);
+	const divisorsOfBigNumber = findDivisors(bigNumber);
+	const answer = divisorsOfBigNumber.reduce((sum, reg) => {
+		return sum + reg;
+	}, 0);
+	return answer;
+}
+
+module.exports = {
+	question1,
+	question2
+}
+
+
